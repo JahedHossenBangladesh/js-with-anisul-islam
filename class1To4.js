36e1beddaa3e46ccd861b8db52c4a4a3eb32f9cd
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-07-06 17:43:35 +0600

    toFixed toString Number typeof pareseFloat parseInt toPrecision

---
+++ class1To4.js
@@ -0,0 +1,19 @@
+var num = 20;
+console.log(num)
+num= toString(num);
+console.log(typeof(num))
+
+num =30.50;
+num = parseFloat(num);
+console.log(num)
+console.log(typeof(num));
+let z = parseInt(num);
+console.log(z)
+z=30.877
+console.log(z.toPrecision(1));
+let y = z.toFixed(2)
+console.log(y)
+
+console.log(typeof(Number('23')));
+
+
